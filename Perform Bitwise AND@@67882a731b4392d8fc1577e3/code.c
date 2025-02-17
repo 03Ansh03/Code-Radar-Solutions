a09d0fd385b87e85a5b121903715d36a602d1d21
Ansh Sachdeva <anshsachdeva2703@gmail.com>
2025-02-17 19:23:33 +0530

    Solution to Perform Bitwise AND

--- Perform Bitwise AND@@67882a731b4392d8fc1577e3/code.c
+++ Perform Bitwise AND@@67882a731b4392d8fc1577e3/code.c
@@ -2,8 +2,8 @@
 #include <stdio.h>
 int main(){
     int a,b;
-    scanf("%d %d",&a,&b);
-    int result=a|b;
-    printf("%d",result);
+    scanf("%d %d ",&a,&b);
 
+    int result = a | b;
+    printf("%d",result);
 }
